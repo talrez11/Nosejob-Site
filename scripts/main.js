447a26af38b108aedcb6a1a54fe9a8a4e5ebe7a8
Eyal Gershon <eyal@way2cu.com>
2015-01-20 13:11:45 +0200

    Main.js: Added leadSent dataLayer event on form submission.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 /**
  * Main JavaScript
- * Site Name
+ * Drnose
  *
  * Copyright (c) 2014. by Way2CU, http://way2cu.com
  * Authors:
@@ -8,6 +8,13 @@
 
 function on_site_load() {
 	Caracal.lightbox = new LightBox('a.image.direct', false, false, true);
+
+	$('form').on('analytics-event', function(event, data) {
+            if (!data.error)
+                    dataLayer.push({
+                    	'event':'leadSent'
+                    });
+    });
 }
 
 $(on_site_load);
