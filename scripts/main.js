a1a3fab14aa851fdbbfdeb5753a6f0382d63c894
Eyal Gershon <eyal@way2cu.com>
2015-01-29 17:29:41 +0200

    Edited header and added video dialog to show on top on click.

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@
  */
 
 function on_site_load() {
-	Caracal.lightbox = new LightBox('a.image.direct', false, false, true);
+	// Caracal.lightbox = new LightBox('a.image.direct', false, false, true);
 
 	$('form').on('analytics-event', function(event, data) {
             if (!data.error)
@@ -15,6 +15,21 @@ function on_site_load() {
                     	'event':'leadSent'
                     });
     });
+
+    // video dialog
+	var video_dialog = new Dialog();
+
+	video_dialog.setTitle(language_handler.getText(null, 'dialog_video_title'));
+	video_dialog.setSize(640, 400);
+
+	$('a.video').not('.mobile').click(function(event) {
+		// prevent link from working
+		event.preventDefault();
+
+		// set content from URL and show it
+		video_dialog.setContentFromURL($(this).attr('href'));
+		video_dialog.showWhenReady();
+	});
 }
 
 $(on_site_load);
